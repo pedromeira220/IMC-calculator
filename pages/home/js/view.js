5913d19f5ea5b4b8dbe74bea27c3efa9520001ca
Pedro Meira <petergamer8k@gmail.com>
2022-05-20 16:06:22 -0300

    Adicionando a situação onde o usuário pode estar de acordo com seu IMC

--- pages/home/js/view.js
+++ pages/home/js/view.js
@@ -13,4 +13,24 @@ const view = {
     showBmiInfoBox() {
         document.querySelector(".bmi-info-box").classList.remove("display-none");
     },
+    bmiInfo: {
+        situation: {
+            "Normal": {
+                text: "Parabéns, você está com o peso normal. Recomendamos que mantenha hábitos saudáveis em seu dia a dia. Especialistas sugerem ingerir de 4 a 5 porções diárias de frutas, verduras e legumes, além da prática de exercícios físicos - pelo menos 150 minutos semanais."
+            },
+            "Sobrepeso": {
+                text: "Atenção! Alguns quilos a mais já são suficientes para que algumas pessoas desenvolvam doenças associadas, como diabetes e hipertensão. É importante rever seus hábitos. Procure um médico."
+
+            },
+            "Obesidade grau I": {
+                text: "Sinal de alerta! O excesso de peso é fator de risco para desenvolvimento de outros problemas de saúde. A boa notícia é que uma pequena perda de peso já traz benefícios à saúde. Procure um médico para definir o tratamento mais adequado para você."
+            },
+            "Obesidade grau II": {
+                text: "Sinal vermelho! Ao atingir este nível de IMC, o risco de doenças associadas está entre alto e muito alto. Busque ajuda de um profissional de saúde; não perca tempo."
+            },
+            "Obesidade grau III": {
+                text: "Sinal vermelho! Ao atingir este nível de IMC, o risco de doenças associadas é muito alto. Busque ajuda de um profissional de saúde; não perca tempo."
+            }
+        }
+    }
 }
